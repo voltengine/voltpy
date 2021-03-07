c4f0703f3a10c133fbb4744483f79620aa4a8356
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2021-03-08 00:16:44 +0100

    Prepare example code

---
+++ src/voltpy/include/voltpy.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace voltpy {
+
+void hello();
+
+}
---
+++ src/voltpy/src/voltpy.cpp
@@ -0,0 +1,11 @@
+#include "voltpy.hpp"
+
+#include <iostream>
+
+namespace voltpy {
+
+void hello() {
+	std::cout << "Hello, voltpy!" << std::endl;
+}
+
+}
